c7e1afa9c113bab00e5e3d77081d8a5196cf3c21
Jan De Rudder <39810873+janderudder@users.noreply.github.com>
2022-12-13 11:56:01 +0100

    doc - revise bash's path.sh style and WSL's readme

--- src/bash/config/path.sh
+++ src/bash/config/path.sh
@@ -1,1 +1,3 @@
-export PATH="$HOME/bin:$PATH:/mnt/c/Users/__WIN_USER_NAME__/AppData/Local/Programs/Microsoft VS Code/bin"
+PATH="$HOME/bin:$PATH"
+PATH="$PATH:/mnt/c/Users/__WIN_USER_NAME__/AppData/Local/Programs/Microsoft VS Code/bin"
+export PATH="$PATH:$HOME/bin/win-bin"
--- src/wsl/readme.md
+++ src/wsl/readme.md
@@ -1,1 +1,5 @@
+# WSL configuration
+
 Copy `wsl.conf` into `/etc/wsl.conf`
+
+This configuration prevents Windows' system folders being added to the path. Rather, I prefer creating soft links to useful Windows' binaries in `$HOME/bin/win-bin`.
