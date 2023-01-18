e7692cf663be0a1b29faba2354675fbeac90d866
Jan De Rudder <39810873+janderudder@users.noreply.github.com>
2023-01-18 08:23:10 +0100

    feat - update git configuration to detect available pager

--- src/git/configuration.sh
+++ src/git/configuration.sh
@@ -1,2 +1,12 @@
-git config --global core.pager '\bat -p --wrap=character --force-colorization'
+git config --global user.name 'Jan De Rudder'
+git config --global user.email '39810873+janderudder@users.noreply.github.com'
 git config --global init.defaultBranch 'main'
+
+if command -v bat > /dev/null 2>&1; then
+  pager='\bat -p --wrap=character --force-colorization'
+elif test -n "$PAGER"; then
+    pager="$PAGER"
+fi
+
+git config --global core.pager "$pager"
+
