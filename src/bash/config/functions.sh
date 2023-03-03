1093a9c81b227b9dc6dd836b6f1b68ece394c3be
Jan De Rudder <39810873+janderudder@users.noreply.github.com>
2023-03-03 11:16:30 +0100

    feat - add bash set_c_compiler function and rename process functions

--- src/bash/config/functions.sh
+++ src/bash/config/functions.sh
@@ -1,12 +1,25 @@
-get_process_pid()
+process_pid()
 {
     pgrep "$1"
     return $?
 }
 
 
-get_process_name()
+process_name()
 {
     ps -p "$1" -o comm=
     return $?
+}
+
+
+set_c_compiler()
+{
+	[[ $1 != gcc && $1 != clang ]] \
+		&& echo Warning, selected compiler is neither GCC nor Clang 1>&2
+	export CC="$1"
+	if [[ $1 == gcc ]]; then
+		export CXX='g++'
+	else
+		export CXX="${1}++"
+	fi
 }
