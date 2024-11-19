961cbb25c9584fb23e5bc8b6b6e38df877ffe4e8
Jan De Rudder <39810873+janderudder@users.noreply.github.com>
2024-11-19 13:07:19 +0100

    feat: add systemd to wsl

--- git/configuration.sh
+++ git/configuration.sh
@@ -3,10 +3,9 @@ git config --global user.email '39810873+janderudder@users.noreply.github.com'
 git config --global init.defaultBranch 'main'
 
 if command -v bat > /dev/null 2>&1; then
-  pager='\bat -p --wrap=character --force-colorization'
+  	pager='\bat -p --wrap=character --force-colorization'
 elif test -n "$PAGER"; then
     pager="$PAGER"
 fi
 
 git config --global core.pager "$pager"
-
